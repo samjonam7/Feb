8204e27cdbfebc07f059a8c51a34e5de9a3ca83c
samjonam7 <samjonam7@gmail.com>
2022-05-23 15:48:48 +0530

    modified

--- src/org/test/GreenBank.java
+++ src/org/test/GreenBank.java
@@ -3,6 +3,7 @@ package org.test;
 public class GreenBank extends AxisBank{
 public void deposite() {
 	System.out.println("10%");
+	System.out.println("12%");
 }
 public static void main(String[] args) {
 	GreenBank g =new GreenBank();
